14acecf79f2d3aeca4e244c66069b6582195bf5b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-13 14:41:49 -0400

    add kuker with redux emitter

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # todomvc-redux-kuker
 
 Example forked from [reduxjs/redux](https://github.com/reduxjs/redux/tree/master/examples/todomvc)
+
+- [kuker](https://github.com/krasimir/kuker)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+import { ReduxEmitter } from 'kuker-emitters'
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import reducer from './reducers'
 import 'todomvc-app-css/index.css'
 
-const store = createStore(reducer)
+const middleware = ReduxEmitter()
+
+const store = createStore(reducer, applyMiddleware(middleware))
 
 render(
   <Provider store={store}>
