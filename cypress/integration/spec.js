3f65b43671487c968a28546619783b5e0cbfca11
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-14 22:40:09 -0400

    add second test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -11,4 +11,28 @@ context('TodoMVC', () => {
       .type('be cool{enter}')
     cy.get('.todo-list li').should('have.length', 2)
   })
+
+  it.only('clears completed', () => {
+    cy.get('.new-todo')
+      .type('first{enter}')
+      .type('second{enter}')
+      .type('third{enter}')
+      .type('fourth{enter}')
+    cy.get('.todo-list li').should('have.length', 4)
+    cy.contains('li', 'second')
+      .find('.toggle')
+      .check()
+    cy.contains('li', 'fourth')
+      .find('.toggle')
+      .check()
+    cy.get('.clear-completed').click()
+    // only two items should remain
+    cy.get('.todo-list li')
+      .should('have.length', 2)
+      .and(li$ => {
+        // make sure the expected items remain
+        expect(li$[0].textContent).to.equal('first')
+        expect(li$[1].textContent).to.equal('third')
+      })
+  })
 })
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types'
 import TodoTextInput from './TodoTextInput'
 
 const Header = ({ addTodo }) => (
-  <header className="header">
+  <header className='header'>
     <h1>todos</h1>
     <TodoTextInput
       newTodo
-      onSave={(text) => {
+      onSave={text => {
         if (text.length !== 0) {
           addTodo(text)
         }
       }}
-      placeholder="What needs to be done?"
+      placeholder='What needs to be done?'
     />
   </header>
 )
